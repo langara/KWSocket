9f3b64ff3df820e4c88d36803520ad0a35d25328
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-08-11 10:39:54 +0200

    Rename example activity for web socket experiments

--- iot-guard-android/mobile/src/main/java/pl/elpassion/iotguard/ExampleWSActivity.kt
+++ iot-guard-android/mobile/src/main/java/pl/elpassion/iotguard/ExampleWSActivity.kt
@@ -2,13 +2,13 @@ package pl.elpassion.iotguard
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.example_ws_activity.*
 
-class MainActivity : AppCompatActivity() {
+class ExampleWSActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.example_ws_activity)
         ChatServer.launch(9999)
         connect.setOnClickListener {
             ExampleClient.launch()
