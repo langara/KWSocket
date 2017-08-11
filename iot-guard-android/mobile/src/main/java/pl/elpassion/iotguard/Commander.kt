d2bcee8a074c319f76ed2f4a84f01a98a2c88044
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-08-11 14:14:05 +0200

    Add commander model

---
+++ iot-guard-android/mobile/src/main/java/pl/elpassion/iotguard/Commander.kt
@@ -0,0 +1,39 @@
+package pl.elpassion.iotguard
+
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+
+
+interface Model<ActionType, StateType> {
+
+    val states: Observable<StateType>
+
+    fun perform(action: ActionType)
+}
+
+interface CommanderModel : Model<CommanderAction, CommanderState>
+
+sealed class CommanderAction
+
+object Forward : CommanderAction()
+object Backward : CommanderAction()
+object Left : CommanderAction()
+object Right : CommanderAction()
+
+sealed class CommanderState
+
+object Disconnected : CommanderState()
+object Connected : CommanderState()
+
+
+
+class CommanderModelImpl : CommanderModel {
+
+    private val statesSubject = BehaviorSubject.createDefault<CommanderState>(Disconnected)
+
+    override val states: Observable<CommanderState> = statesSubject
+
+    override fun perform(action: CommanderAction) {
+        println("TODO: perform action: $action")
+    }
+}
--- iot-guard-android/mobile/src/main/java/pl/elpassion/iotguard/CommanderActivity.kt
+++ iot-guard-android/mobile/src/main/java/pl/elpassion/iotguard/CommanderActivity.kt
@@ -1,12 +1,38 @@
 package pl.elpassion.iotguard
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle2.kotlin.bindToLifecycle
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.commander_activity.*
 
-class CommanderActivity : AppCompatActivity() {
+class CommanderActivity : RxAppCompatActivity() {
+
+    private val model by lazy { DI.provideCommanderModel() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.commander_activity)
+        initModel()
+        forward.setOnClickListener { model.perform(Forward) }
+        backward.setOnClickListener { model.perform(Backward) }
+        left.setOnClickListener { model.perform(Left) }
+        right.setOnClickListener { model.perform(Right) }
+    }
+
+    protected fun initModel() {
+        model.states
+                .bindToLifecycle(this)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if (!isFinishing) {
+                        showState(it)
+                    }
+                }
+    }
+
+    private fun showState(state: CommanderState?) {
+        Log.w("IoT Commander Activity", "TODO: show state: $state")
     }
 }
