beea711703c699c137fa325a65e4fa07a92103d7
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-08-11 10:54:51 +0200

    Add commander activity

---
+++ iot-guard-android/mobile/src/main/java/pl/elpassion/iotguard/CommanderActivity.kt
@@ -0,0 +1,12 @@
+package pl.elpassion.iotguard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CommanderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.commander_activity)
+    }
+}
