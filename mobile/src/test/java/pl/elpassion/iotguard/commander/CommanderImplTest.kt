89b0b0a26bc2ae8583d96ba4468b1e0b5802c749
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-08-16 14:41:15 +0200

    Add tests for connecting/disconnecting commander to/from robot

--- mobile/src/test/java/pl/elpassion/iotguard/commander/CommanderImplTest.kt
+++ mobile/src/test/java/pl/elpassion/iotguard/commander/CommanderImplTest.kt
@@ -5,25 +5,37 @@ package pl.elpassion.iotguard.commander
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import io.reactivex.subjects.PublishSubject
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import pl.elpassion.iotguard.SimpleLogger
-import pl.elpassion.iotguard.api.Client
-import pl.elpassion.iotguard.api.Socket
+import pl.elpassion.iotguard.api.*
 import pl.elpassion.iotguard.commander.CommanderAction.*
+import pl.elpassion.iotguard.commander.CommanderState.Connected
+import pl.elpassion.iotguard.commander.CommanderState.Disconnected
 
 class CommanderImplTest {
 
+    val events = PublishSubject.create<Event>()
     val socket = mock<Socket>()
+    val client = mock<Client>()
+    val observer = TestObserver<CommanderState>()
 
-    val client = mock<Client>().apply {
-        whenever(connections).thenReturn(listOf(socket))
-        whenever(events).thenReturn(Observable.empty())
+    init {
+        whenever(client.connections).thenReturn(listOf(socket))
+        whenever(client.events).thenReturn(events)
     }
 
     val logger = SimpleLogger()
 
     val commander = CommanderImpl(client, logger)
+
+    @Before
+    fun setup() {
+        commander.states.subscribe(observer)
+    }
 
     @Test
     fun `Connect to websocket client`() {
@@ -62,4 +74,26 @@ class CommanderImplTest {
         verify(socket).send("stop")
     }
 
+    @Test
+    fun `Disconnected state by default`() {
+        observer.assertValue(Disconnected)
+    }
+
+    @Test
+    fun `Change to connected state when connection opens`() {
+        events.onNext(Open())
+        observer.assertLastValue(Connected)
+    }
+
+    @Test
+    fun `Change to disconnected state when connection closes`() {
+        events.onNext(Open())
+        events.onNext(Close(0))
+        observer.assertLastValue(Disconnected)
+    }
+}
+
+
+fun <T> TestObserver<T>.assertLastValue(value: T) {
+    assertEquals(value, values().last())
 }
