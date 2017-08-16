8f78cb8ae998a82eac5f32f63cc8fbf8d885aec6
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-08-16 10:53:12 +0200

    [refactor] Move Endpoint.messages observable out of the Endpoint interface itself

--- mobile/src/main/java/pl/elpassion/iotguard/api/Api.kt
+++ mobile/src/main/java/pl/elpassion/iotguard/api/Api.kt
@@ -21,9 +21,10 @@ object Start : Event()
 interface Endpoint : AutoCloseable {
     val connections: List<Socket> // client will have at most one. server can have many
     val events: Observable<Event>
-    val messages: Observable<String> get() = events.ofType(Message::class.java).map { it.message }
     fun send(message: String) = connections.forEach { it.send(message) }
 }
+
+val Endpoint.messages: Observable<String> get() = events.ofType(Message::class.java).map { it.message }
 
 interface Client : Endpoint {
     fun connect(serverURI: String)
--- mobile/src/main/java/pl/elpassion/iotguard/example/ExampleWSActivity.kt
+++ mobile/src/main/java/pl/elpassion/iotguard/example/ExampleWSActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.example_ws_activity.*
 import pl.elpassion.iotguard.DI
 import pl.elpassion.iotguard.R
+import pl.elpassion.iotguard.api.messages
 
 class ExampleWSActivity : AppCompatActivity() {
 
