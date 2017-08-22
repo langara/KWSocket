f320c8ab986653d337aee7e0f2479cd62e6e9413
Paweł Gajda <pawegio@gmail.com>
2017-08-22 14:22:08 +0200

    [refactoring] Extract fun updating renderer

--- mobile/src/main/java/pl/elpassion/iotguard/streaming/RtcListener.kt
+++ mobile/src/main/java/pl/elpassion/iotguard/streaming/RtcListener.kt
@@ -14,18 +14,23 @@ class RtcListener(private val activity: Activity,
     override fun onLocalStream(localStream: MediaStream) {
         super.onLocalStream(localStream)
         activity.runOnUiThread {
-            localStream.videoTracks[0].addRenderer(VideoRenderer(localRenderer)) }
+            localStream.videoTracks[0].addRenderer(VideoRenderer(localRenderer))
+        }
     }
 
     override fun onAddRemoteStream(remoteStream: MediaStream, peer: PnPeer) {
         super.onAddRemoteStream(remoteStream, peer)
         activity.runOnUiThread {
             remoteStream.videoTracks[0].addRenderer(VideoRenderer(remoteRenderer))
-            VideoRendererGui.update(remoteRenderer, 0, 0, 100, 100,
-                    VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, false)
-            VideoRendererGui.update(localRenderer, 72, 72, 25, 25,
-                    VideoRendererGui.ScalingType.SCALE_ASPECT_FIT, true)
+            updateRenderer(remoteRenderer, 0, 0, 100, 100)
+            updateRenderer(localRenderer, 72, 72, 25, 25)
         }
+    }
+
+    private fun updateRenderer(renderer: VideoRenderer.Callbacks?,
+                               x: Int, y: Int, width: Int, height: Int) {
+        val scalingType = VideoRendererGui.ScalingType.SCALE_ASPECT_FILL
+        VideoRendererGui.update(renderer, x, y, width, height, scalingType, false)
     }
 
     override fun onMessage(peer: PnPeer?, message: Any?) {
