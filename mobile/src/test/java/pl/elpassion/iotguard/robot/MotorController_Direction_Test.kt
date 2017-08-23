bd1531686b38c2e03ffd2e5d834ad7fd8ddfa8f4
Dominik <dominik.malantowicz@gmail.com>
2017-08-23 12:31:15 +0200

    Set left motor direction to backward in (-180,-135)

---
+++ mobile/src/test/java/pl/elpassion/iotguard/robot/MotorController_Direction_Test.kt
@@ -0,0 +1,28 @@
+package pl.elpassion.iotguard.robot
+
+import org.junit.Assert
+import org.junit.Test
+
+class MotorController_Direction_Test {
+
+    enum class DIRECTION {
+        BACKWARD,
+        FORWARD,
+        NOT_SET
+    }
+
+    private var leftMotorDirection = DIRECTION.NOT_SET
+    private val changeLeftMotorToBackward = { leftMotorDirection = DIRECTION.BACKWARD }
+    private val changeLeftMotorToForward = { leftMotorDirection = DIRECTION.FORWARD }
+
+    @Test
+    fun `Should set left motor direction to backward in (-180,-135) `() {
+        setMotorDirections(changeLeftMotorToBackward)
+        Assert.assertEquals(leftMotorDirection, DIRECTION.BACKWARD)
+    }
+
+    private fun setMotorDirections(changeLeftMotorToBackward: () -> Unit) {
+        changeLeftMotorToBackward()
+    }
+
+}
