60afee6ddc45399ca2b275379185a2c8bf9de8f0
Dominik <dominik.malantowicz@gmail.com>
2017-08-23 12:39:24 +0200

    Set left motor direction to forward in (-180, -135)

--- mobile/src/test/java/pl/elpassion/iotguard/robot/MotorController_Direction_Test.kt
+++ mobile/src/test/java/pl/elpassion/iotguard/robot/MotorController_Direction_Test.kt
@@ -1,28 +1,45 @@
 package pl.elpassion.iotguard.robot
 
-import org.junit.Assert
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class MotorController_Direction_Test {
 
-    enum class DIRECTION {
-        BACKWARD,
-        FORWARD,
-        NOT_SET
-    }
-
-    private var leftMotorDirection = DIRECTION.NOT_SET
-    private val changeLeftMotorToBackward = { leftMotorDirection = DIRECTION.BACKWARD }
-    private val changeLeftMotorToForward = { leftMotorDirection = DIRECTION.FORWARD }
+    private val changeLeftMotorToBackward = mock<() -> Unit>()
+    private val changeLeftMotorToForward = mock<() -> Unit>()
 
     @Test
     fun `Should set left motor direction to backward in (135, 180) `() {
-        setMotorDirections(changeLeftMotorToBackward)
-        Assert.assertEquals(leftMotorDirection, DIRECTION.BACKWARD)
+        setMotorDirections(
+                degree = 140,
+                changeLeftMotorToBackward = changeLeftMotorToBackward,
+                changeLeftMotorToForward = mock())
+
+        verify(changeLeftMotorToBackward).invoke()
     }
 
-    private fun setMotorDirections(changeLeftMotorToBackward: () -> Unit) {
-        changeLeftMotorToBackward()
+    @Test
+    fun `Should set left motor direction to forward in (-180, -135) `() {
+
+        setMotorDirections(
+                degree = -140,
+                changeLeftMotorToBackward = changeLeftMotorToBackward,
+                changeLeftMotorToForward = changeLeftMotorToForward)
+
+        verify(changeLeftMotorToForward).invoke()
+    }
+
+    private fun setMotorDirections(
+            degree: Int,
+            changeLeftMotorToBackward: () -> Unit,
+            changeLeftMotorToForward: () -> Unit) {
+
+        if (degree < 0) {
+            changeLeftMotorToForward()
+        } else {
+            changeLeftMotorToBackward()
+        }
     }
 
 }
