17c294e3322517de897b12199cfaec948e8ace6e
Dominik <dominik.malantowicz@gmail.com>
2017-09-04 16:54:05 +0200

    Add if to stop even when two pins have high value

--- arduino/cameraController.ino
+++ arduino/cameraController.ino
@@ -3,27 +3,33 @@
 Servo servo;
 
 int pos = 180;
-int goLeftPin = 12;
-int goRightPin = 13;
+int goLeftPin = 13;
+int goRightPin = 12;
+int goRight = 0;
+int goLeft = 0;
 
 void setup() {
- servo.attach(9);
- pinMode(goLeftPin, INPUT);  
- pinMode(goRightPin, INPUT);
- servo.write(pos); 
- Serial.begin(9600);
+    servo.attach(9);
+    pinMode(goLeftPin, INPUT);
+    pinMode(goRightPin, INPUT);
 }
- 
+
 void loop() {
-  if (digitalRead(goRightPin)) {
-      Serial.write("Go-right\n");
-      pos = pos - 2;
-      servo.write(pos);
-      
-  } else if (digitalRead(goLeftPin)) {
-      Serial.write("Go-left\n");
-      pos = pos + 2;
-      servo.write(pos);
+  goRight = digitalRead(goRightPin);
+  goLeft = digitalRead(goLeftPin);
+
+  if (goRight != goLeft) {
+     if (goRight) {
+        if (pos > 60){
+            pos = pos - 1;
+            servo.write(pos);
+          }
+     } else if (goLeft) {
+        if (pos < 180) {
+            pos = pos + 1;
+            servo.write(pos);
+        }
+    }
   }
-  delay(50);
+  delay(25);
 }
